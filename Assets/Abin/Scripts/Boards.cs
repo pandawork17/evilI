7954521015ced5176d3dc982c463e0ebe504c272
Abin Bae <baeabin903@gmail.com>
2024-11-08 16:37:37 +0900

    33맵(1)

---
+++ Assets/Abin/Scripts/Boards.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boards : MonoBehaviour
+{
+    public int width;
+    public int height;
+    private BackgroundTile[,] allTiles;
+    public GameObject tilePrefab;
+    void Start()
+    {
+        allTiles = new BackgroundTile[width,height];
+        SetUp();
+    }
+
+    private void SetUp(){
+        for ( int i = 0;i<width; i ++){
+            for (int j = 0; j<height; j++){
+                Vector2 tempPosition = new Vector2(i+1,j+1);
+                Instantiate(tilePrefab, tempPosition,Quaternion.identity);
+            }
+        }
+    }
+
+
+}
