373121cd61d8b2914bfabadabaf78342fd23dd52
Abin Bae <baeabin903@gmail.com>
2024-11-20 15:47:34 +0900

    fix

--- Assets/Abin/Scripts/BackgroundTile.cs
+++ Assets/Abin/Scripts/BackgroundTile.cs
@@ -21,7 +21,7 @@ public class BackgroundTile : MonoBehaviour
         int dotToUse = Random.Range(0,dots.Length);
         GameObject dot = Instantiate(dots[dotToUse],transform.position, Quaternion.identity);
         dot.transform.parent = this.transform;
-        dot.name = dot.gameObject.name;
+        dot.name = this.gameObject.name;
 
     }
 }
